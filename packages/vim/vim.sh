23eb9314b276ba957a50f74b784239280a3b7d0e
simonVansuyt <simon.vansuyt@gmail.com>
2017-01-14 08:57:18 +0100

    vim.sh check if there is a .vim and .vimrc

--- packages/vim/vim.sh
+++ packages/vim/vim.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 if type vim >/dev/null 2>/dev/null; then
   cp -f .vimrc .vim ~
-  echo -e "Vim is ready to go"
+  if [ -f ~/.vimrc ] && [ -d ~/.vim ]; then
+    echo -e "Vim is ready to go"
+  else
+    echo -e "/x1B[0;32m Sorry somthing went wrong /x1B[0m"
+  fi
 fi
