e3f6c5e0b4a6b60a5f357f74537393eaa5d9a629
simonVansuyt <simon.vansuyt@gmail.com>
2020-08-02 15:54:38 +0200

    added backup option

--- dfiles/dfiles.sh
+++ dfiles/dfiles.sh
@@ -1,32 +1,65 @@
 #!/bin/bash
-
-# moving the files to homedirectory
-for file in .{bash_profile,bashrc,bash_prompt,aliases,exports,functions,extra}; do
-  # adding while so you don't skip some importan files
-  while true; do
+# this script makes a backup of the dotfiles and install the new
+# backup current files
+while true; do
     # asking the question 
-    echo -e "\x1B[0;33m"
-    read -r -p "Do you want to overwrite ${file}? [Y/n] " resp
-    echo -e "\x1B[0m"
+    echo -e "\x1B[0;33m" 
+    read -r -p "Do you want to backup the current  ${file}? [Y/n] " resp
+    echo -e "\x1B[0m" # white
 
     case $resp in
-      [yY])
-	# he said yes
-	echo -e "\x1B[0;32m [*] \x1B[0mAllons-y, Geronimo!"
-	# moving the file
-	cp -f ./dfiles/${file} ~/${file}
-	break
-	;;
-      [nN])
-	# he said no
-	echo -e "\x1B[0;31m [*] \x1B[0mThese aren't the droids we're looking for, move along..."
-	break;;
-      *) 
-	# he said idk, so lets repeat
-	echo -e "\x1B[0;33m [*] \x1B[0mWhat did you say? Try again please! [Y/n]"
-	;;
+        [yY])
+            # he said yes
+            echo -e "\x1B[0;33m [*] \x1B[0mCopy files to ~/.backup"
+            # check if there isn't exist a backup folder
+            if [ ! -d "~/.backup" ]
+            then
+                # Create a .backup file
+                echo -e "Creating a the directory ~/.backup"
+                mkdir ~/.backup
+            fi
+            for file in .{bash_profile,bashrc,bash_prompt,aliases,exports,functions,extra}; do
+                # moving the files
+                cp -f ~/${file} ~/.backup/${file}
+                echo -e "\x1B[0;32m [*] \x1B[0mAllons-y copied ${file}!"
+            done
+            break;;
+        [nN])
+            # he said no
+            echo -e "\x1B[0;31m [*] \x1B[0mThese aren't the droids we're looking for, move along..."
+            break;;
+        *) 
+            # he said idk, so lets repeat
+            echo -e "\x1B[0;33m [*] \x1B[0mWhat did you say? Try again please! [Y/n]"
+            ;;
     esac
-  done
+done
+# moving the files to homedirectory
+for file in .{bash_profile,bashrc,bash_prompt,aliases,exports,functions,extra}; do
+    # adding while so you don't skip some important files
+    while true; do
+        # asking the question 
+        echo -e "\x1B[0;33m" 
+        read -r -p "Do you want to overwrite ${file}? [Y/n] " resp
+        echo -e "\x1B[0m" # white
+
+        case $resp in
+            [yY])
+                # he said yes
+                echo -e "\x1B[0;32m [*] \x1B[0mAllons-y, Geronimo!"
+                # moving the file
+                cp -f ./${file} ~/${file}
+                break;;
+            [nN])
+                # he said no
+                echo -e "\x1B[0;31m [*] \x1B[0mThese aren't the droids we're looking for, move along..."
+                break;;
+            *) 
+                # he said idk, so lets repeat
+                echo -e "\x1B[0;33m [*] \x1B[0mWhat did you say? Try again please! [Y/n]"
+                ;;
+        esac
+    done
 done
 
 source ~/.bash_profile
